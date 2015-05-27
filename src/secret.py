35167d3427496a879f0e352b9ee24d3f476df0af
alex <abondar1992@gmail.com>
2015-05-28 01:49:27 +0300

    Working with Images + hashing exapmles

--- src/secret.py
+++ src/secret.py
@@ -14,6 +14,12 @@ import json
 import pprint
 from collections import defaultdict
 import glob
+from PIL import Image
+import PIL.ExifTags
+import os
+import hashlib
+import hmac
+
 __author__ = "alex"
 __date__ = "$May 15, 2015 7:31:22 PM$"
 
@@ -191,6 +197,50 @@ if __name__ == "__main__":
 #       for c in 'USD','GBP','EUR':
 #         print (c,rates[c])
   
+   #ex14 - images(used images from first examples,requires pillow lib)
+#   pix = Image.open("1drachmi_1973.jpg")
+#   print (pix)
+#   pix.save("test.tiff")
+#   print (pix.info.keys())
+#   pix = Image.open("Common_face_of_one_euro_coin.jpg")
+#   exif = pix._getexif()
+#   print(exif.keys())
+#   
+#   for k,v in pix._getexif().items():
+#       print(PIL.ExifTags.TAGS[k],v)
+#       
+       
+   #ex15 - resizing images and creating thumb copy
+#   for filename in glob.glob("*.jpg"):
+#       name,ext = os.path.splitext(filename)
+#       if name.endswith("_thumb"):
+#           continue
+#       img = Image.open(filename)
+#       thumb = img.copy()
+#       w,h = img.size
+#       largest= max(w,h)
+#       w_n,h_n = w*128//largest, h*128//largest
+#       print ("Resize",filename,"from",w,h,"to",w_n,h_n)
+#       thumb.thumbnail((w_n,h_n),PIL.Image.ANTIALIAS) #resample PIL.Image.ANTIALIAS
+#       thumb.save(name+"_thumb"+ext)
+# 
    
-   
-   
+   #ex16 - cropping images
+#   pix = Image.open("DSC_0811.JPG")
+#   print (pix.size)
+#   w,h = pix.size
+#   pix.crop(box=(w//3,0,2*2//3,h//3)).show()
+#   pix.crop(box=(w//3,h//3,2*2//3,h//3)).show()
+#   
+# 
+  #ex17 hashing
+#  md5 = hashlib.new("md5")
+#  with open ("DSC_0811.JPG","rb") as some_file:
+#      md5.update(some_file.read())
+#  print (md5.hexdigest())
+
+  #ex18 adding key to hashing
+  with open ("DSC_0811.JPG","rb") as some_file:
+      keyed = hmac.new( b"Bibizyan Govnokoder",some_file.read() )
+  print (keyed.hexdigest())    
+  
